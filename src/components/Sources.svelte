475306c0bface8f59673728abc6233550f0e68a9
Zoe Ludena <zludena@ucsd.edu>
2024-03-07 21:24:04 -0800

    Adding our sources

--- src/components/Sources.svelte
+++ src/components/Sources.svelte
@@ -36,31 +36,19 @@
         display: flex;
         justify-content: center;
     }
-  
-    pre code {
-        text-align: left; /* Ensure code is left-aligned */
-    }
-  
-    ul {
-        list-style-type: disc; /* Set the bullet style */
-        text-align: left;
-        padding-left: 0; /* Remove default padding */
-    }
-  
-    ul ul {
-          list-style-type: circle; /* Change the bullet style for the nested list */
-          text-align: left;
-          margin-top: 5px; /* Adjust the spacing between the parent and nested lists */
-          margin-left: 40px;
-      }
-  
-    li {
-        margin-left: 20px; /* Adjust the space between bullet and text */
-    }
   </style>
     
   <main class="container">
       <h1>Sources</h1>
-      <p style="text-align: center;">Great work! Click on the donut to go back to the home page!</p>
+      <p>We used a variety of sources to choose examples, provide accurate information, and to make our visualizations better.</p>
+      <p>AndrewBadr. "Time Complexity." <a href="https://wiki.python.org/moin/TimeComplexity">Time Complexity - Python Wiki</a>, 1 Jan. 2023</p>
+      <p>"Big O Notation in Data Structure: An Introduction: Simplilearn." <a href="https://www.simplilearn.com/big-o-notation-in-data-structure-article">Big O Notation in Data Structures</a>, Simplilearn, 6 Nov. 2023</p>
+      <p>"Big O Notation." <a href="https://en.wikipedia.org/wiki/Big_O_notation">Wikipedia</a>, Wikimedia Foundation, 5 Mar. 2024</p>
+      <p>Huang, Shen. "What Is Big O Notation Explained: Space and Time Complexity." <a href="https://www.freecodecamp.org/news/big-o-notation-why-it-matters-and-why-it-doesnt-1674cfa8a23c/">freeCodeCamp.Org</a>, freeCodeCamp.org, 8 Dec. 2022</p>
+      <p>Prado, Kelvin Salton do. <a href="https://towardsdatascience.com/understanding-time-complexity-with-python-examples-2bda6e8158a7">Understanding Time Complexity with Python Examples</a>, Towards Data Science, 4 Mar. 2019</p>
+      <p>Rowell, Eric. <a href="https://www.bigocheatsheet.com/">Know Thy Complexities!</a> Big-O Algorithms, 2016</p>
+      <p>"Web Safe Color Chart." <a href="https://htmlcolorcodes.com/color-chart/web-safe-color-chart/">HTML Color Codes</a>, htmlcolorcodes.com. Accessed 28 Feb. 2024.</p>
+      
+      <p style="text-align: center;">Click on the donut to go back to the home page!</p>
       <img src={donutWaveLeft} alt="Donut Image" style="width: 150px;display: block; margin-left: auto; margin-right: auto;" on:click={handleDonutClick}>
 </main>
